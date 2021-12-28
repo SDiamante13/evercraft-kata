71ec8a2d1e90625ec1c2b57feeaea3fcac3af276
Steven Diamante <StevenDiamante13@gmail.com>
2021-12-27 23:33:37 -0500

    [Closes #3] Has Default Armor class and hit points.

--- src/main/kotlin/tech/pathtoprogramming/evercraft/model/Character.kt
+++ src/main/kotlin/tech/pathtoprogramming/evercraft/model/Character.kt
@@ -1,6 +1,11 @@
 package tech.pathtoprogramming.evercraft.model
 
-data class Character(val name: String, val alignment: Alignment)
+data class Character(
+   val name: String,
+   val alignment: Alignment,
+   val armorClass: Int = 10,
+   val hitPoints: Int = 5
+)
 
 enum class Alignment {
    GOOD, NEUTRAL, EVIL
--- src/test/kotlin/tech/pathtoprogramming/evercraft/model/CharacterTest.kt
+++ src/test/kotlin/tech/pathtoprogramming/evercraft/model/CharacterTest.kt
@@ -9,9 +9,25 @@ class CharacterTest {
 
     @Test
     fun character_shouldReturnCharacterProperties() {
-        val character = Character("Drew", GOOD);
-        
+        val character = Character("Drew", GOOD, 15, 10);
+
         assertEquals("Drew", character.name);
         assertEquals("GOOD", character.alignment.name);
+        assertEquals(15, character.armorClass);
+        assertEquals(10, character.hitPoints);
+    }
+
+    @Test
+    fun armorClassShouldReturn10ByDefault() {
+        val character = Character("Drew", NEUTRAL);
+
+        assertEquals(10, character.armorClass)
+    }
+
+    @Test
+    fun hitPointsShouldReturn5ByDefault() {
+        val character = Character("Drew", EVIL);
+
+        assertEquals(5, character.hitPoints)
     }
 }
