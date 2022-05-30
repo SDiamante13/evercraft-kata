b9793139319475653cc45d8efc64155891e838e6
Steven Diamante <stevendiamante13@gmail.com>
2022-05-30 15:08:03 -0400

    A combatant can deal critical damage.

--- src/main/java/tech/pathtoprogramming/evercraft/model/Battle.java
+++ src/main/java/tech/pathtoprogramming/evercraft/model/Battle.java
@@ -4,6 +4,8 @@ import tech.pathtoprogramming.evercraft.TwentySidedDie;
 
 public class Battle {
 
+    public static final int CRITICAL_HIT = 20;
+
     private final TwentySidedDie twentySidedDie;
 
     public Battle(TwentySidedDie twentySidedDie) {
@@ -13,7 +15,19 @@ public class Battle {
     public void recordAttack(Character combatant, Character enemyCombatant) {
         int roll = twentySidedDie.roll();
 
-        if (enemyCombatant.isHit(roll)) { // feature envy?
+        recordDamageForSuccessfulAttack(enemyCombatant, roll);
+
+        recordCriticalDamage(enemyCombatant, roll);
+    }
+
+    private void recordDamageForSuccessfulAttack(Character enemyCombatant, int roll) {
+        if (enemyCombatant.isHit(roll)) {
+            enemyCombatant.takeDamage();
+        }
+    }
+
+    private void recordCriticalDamage(Character enemyCombatant, int roll) {
+        if (roll == CRITICAL_HIT) {
             enemyCombatant.takeDamage();
         }
     }
--- src/test/java/tech/pathtoprogramming/evercraft/model/ABattleShould.java
+++ src/test/java/tech/pathtoprogramming/evercraft/model/ABattleShould.java
@@ -1,7 +1,6 @@
 package tech.pathtoprogramming.evercraft.model;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import tech.pathtoprogramming.evercraft.TwentySidedDie;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +19,7 @@ class ABattleShould {
     private static final int MISSED_ROLL = 3;
     private static final int HIT_ROLL = 10;
     public static final int ENEMY_HIT_POINTS = 5;
+    private static final int CRITICAL_HIT_ROLL = 20;
 
     private final TwentySidedDie twentySidedDie = mock(TwentySidedDie.class);
     private final Battle battle = new Battle(twentySidedDie);
@@ -46,5 +46,17 @@ class ABattleShould {
         battle.recordAttack(combatant, enemyCombatant);
 
         assertThat(enemyCombatant.hitPoints()).isEqualTo(ENEMY_HIT_POINTS - 1);
+    }
+
+    @Test
+    void reduceDoubleTheHitPointsForACriticalHit() {
+        when(twentySidedDie.roll())
+                .thenReturn(CRITICAL_HIT_ROLL);
+        Character combatant = new Character("Combatant", Alignment.GOOD);
+        Character enemyCombatant = new Character("Enemy", Alignment.EVIL, 10, ENEMY_HIT_POINTS);
+
+        battle.recordAttack(combatant, enemyCombatant);
+
+        assertThat(enemyCombatant.hitPoints()).isEqualTo(ENEMY_HIT_POINTS - 2);
     }
 }
